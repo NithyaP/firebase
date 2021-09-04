246f3f046b48c72f101a607c46afc855cc16f23f
nithya <nithyapanniyan.gmailo.com>
2021-09-04 12:57:01 +0800

    whatsapp button

--- public/app.js
+++ public/app.js
@@ -1,25 +1,50 @@
 document.addEventListener("DOMContentLoaded",event =>{
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const productId = urlParams.get('productId')
+    
+    console.log(productId);
+
     const app= firebase.app();
     let filterVar = ["toy","bat","electric","mid aged","balls","dolls","vehicles","baby","flying","toy set"];
     // let filterVar = getListData()
-    render_dropdown(filterVar)
-    getCardData(filterVar)
+    
+    if (productId) {
+      getCardData(filterVar, productId)
+    } else {
+      render_dropdown(filterVar)
+      getCardData(filterVar)
+    }
+    
 });
 
 function getListData(){
 
 }
 
-function getCardData(filterVar){
+function getCardData(filterVar, productId = null){
+  $('.navbar div').removeClass('show');
   const db = firebase.firestore();
-  const getCards = db.collection('toys').where("category", "in", filterVar) ;
-  
+  console.log(productId)
+  if (productId) {
+    param1 = "productId"
+    condition = "=="
+    param2 = Number(productId)
+  } else {
+    param1 = "category"
+    condition = "in"
+    param2 = filterVar
+  }
+
+  const getCards = db.collection('toys').where(param1, condition, param2) ;
+
   let cardContainer = document.getElementById("card-container");
   removeAllChildNodes(cardContainer)
 
   getCards.get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
+            console.log("no doc??")
             console.log(doc);
             const data = doc.data();
             console.log(data);
@@ -29,7 +54,6 @@ function getCardData(filterVar){
 }
 
 function render_card(data){
-    
     let cardContainer = document.getElementById("card-container");
     
     let card = document.createElement('div');
@@ -52,19 +76,30 @@ function render_card(data){
     price.innerText = data.price;
     price.id = 'card-price';
 
-    let btn = document.createElement('button');
-    btn.innerText =" Add to cart";
-    btn.className = "btn-lg";
+    wBtn = render_whatsapp_btn(data.productId)
 
     cardBody.appendChild(title);
     cardBody.appendChild(price);
     card.appendChild(imgcard);
     card.appendChild(cardBody);
-    card.appendChild(btn);
+    card.appendChild(wBtn);
     cardContainer.appendChild(card);
 }
 
+function render_whatsapp_btn(productId){
+  let btn = document.createElement('a');
+  btn.setAttribute("role", "button");
+  btn.ariaPressed = "true"
+  var base_url = window.location.origin;
+  var productLink = base_url+"?productId="+productId
+  btn.href = "https://wa.me/6596452181?text=I'm%20interested%20in%20this%20product%20"+productLink+"%20, Pls call me back"
+  btn.innerText ="Contact us for details";
+  btn.className = "btn-lg btn active";
+  return btn
+}
+
 function render_dropdown(data){
+  console.log(data)
   let dropdownElem = document.getElementById("dropdownElem");
 
   data.forEach(element => {
