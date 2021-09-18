831f141ae57345c684011ef9938e5667bc1233f6
vijesh koolothumkandy <vkk@vijeshs-MacBook-Air.local>
2021-09-18 20:47:12 +0800

    phone,whatsapp added

--- public/app.js
+++ public/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded",event =>{
     console.log(productId);
 
     const app= firebase.app();
-    let filterVar = ["toy","bat","electric","mid aged","balls","dolls","vehicles","baby","flying","toy set"];
+    let filterVar = ["electric","balls","dolls","vehicles","flying","toyset", "newborn", "costumes", "cosmetics", "others"];
     // let filterVar = getListData()
     
     if (productId) {
@@ -77,26 +77,54 @@ function render_card(data){
     details.className = 'card-text';
     details.id = 'card-details';
 
+    let btnLgDiv = document.createElement('div');
+    btnLgDiv.className = "btn-lg btn disable card-button-area"
+
+    let btnText = document.createElement('p');
+    btnText.className = "card-text btn-text"
+    btnText.innerText = "Contact us for details"
+
     wBtn = render_whatsapp_btn(data.productId)
+    pBtn = render_phone_btn()
+
+    btnLgDiv.appendChild(btnText)
+    btnLgDiv.appendChild(wBtn)
+    btnLgDiv.appendChild(pBtn)
 
     cardBody.appendChild(title);
     cardBody.appendChild(details);
     card.appendChild(imgcard);
     card.appendChild(cardBody);
-    card.appendChild(wBtn);
+    card.appendChild(btnLgDiv);
     cardContainer.appendChild(card);
+}
+
+function render_phone_btn(){
+  let btn = document.createElement('a');
+  btn.className = "card-icons-cust"
+  var base_url = window.location.origin;
+  var productLink = base_url
+  btn.href = "tel:916238905264"
+  
+  let img = document.createElement('img');
+  img.src = "images/phone.png";
+
+  btn.appendChild(img);
+  return btn
 }
 
 function render_whatsapp_btn(productId){
   let btn = document.createElement('a');
-  btn.setAttribute("role", "button");
-  btn.ariaPressed = "true"
+  btn.className = "card-icons-cust"
   var base_url = window.location.origin;
   var productLink = base_url+"?productId="+productId
-  // btn.href = "https://wa.me/916238905264?text=I'm%20interested%20in%20this%20product%20"+productLink+"%20, Pls call me back"
-  btn.href = "https://wa.me/6596452181?text=I'm%20interested%20in%20this%20product%20"+productLink+"%20, Pls call me back"
-  btn.innerText ="Contact Us for details";
-  btn.className = "btn-lg btn active";
+  btn.href = "https://wa.me/916238905264?text=I'm%20interested%20in%20this%20product%20"+productLink+"%20, Pls call me back"
+  // btn.href = "https://wa.me/6596452181?text=I'm%20interested%20in%20this%20product%20"+productLink+"%20, Pls call me back"
+  
+  let img = document.createElement('img');
+  img.src = "images/whatsapp.png";
+
+  btn.appendChild(img);
   return btn
 }
 
